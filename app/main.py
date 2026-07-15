b10f6d5a122089395ed99351b51b3f25b91c3d0c
Elisiane Quadros <elisiane.quadros@hotmail.com>
2026-07-15 19:54:21 -0300

    style: quebra linha longa do with open() para conformidade com PEP8

--- app/main.py
+++ app/main.py
@@ -10,7 +10,13 @@ def copy_file(command: str) -> None:
     if source_file_name == target_file_name:
         return
     try:
-        with open(source_file_name, "r") as source_file, open(target_file_name, "w") as target_file:
+        with open(
+            source_file_name,
+            "r",
+        ) as source_file, open(
+            target_file_name,
+            "w",
+        ) as target_file:
             target_file.write(source_file.read())
     except FileNotFoundError:
         return
