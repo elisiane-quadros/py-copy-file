73b7de569f6444ab682baca78d6ae917b1fe1a39
Elisiane Quadros <elisiane.quadros@hotmail.com>
2026-07-15 16:00:59 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+
+    source_file = parts[1]
+    target_file = parts[2]
+
+    if source_file == target_file:
+        return
+
+    try:
+        with open(source_file, "r") as sf, open(target_file, "w") as tf:
+            tf.write(sf.read())
+    except FileNotFoundError:
+        return
